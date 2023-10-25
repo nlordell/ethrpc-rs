588f349370aa79b0ea028200349bc98e36e74059
Benjamin Smith <bh2smith@users.noreply.github.com>
2023-10-25 12:04:42 +0200

    Add Moar RPC Methods & New Tx Type (#1)
    This includes all the contents from the following PRs in
    [ethcrates](https://github.com/nlordell/ethprim-rs):
    
    - getCode: https://github.com/nlordell/ethprim-rs/pull/2
    - getTransactionCountBy***:
    https://github.com/nlordell/ethprim-rs/pull/3
    - getTransactionBy*: https://github.com/nlordell/ethprim-rs/pull/4
    
    ---------
    
    Co-authored-by: Nicholas Rodrigues Lordello <n@lordello.net>

--- src/lib.rs
+++ src/lib.rs
@@ -45,13 +45,33 @@ module! {
         pub struct GetBlockByNumber as "eth_getBlockByNumber"
             (BlockSpec, Hydrated) => Option<Block>;
 
-        /// Returns a collection of all logs matching the given filter.
-        pub struct GetLogs as "eth_getLogs"
-            (LogFilter,) => Vec<Log>;
+        /// Returns the number of transactions in a block from a block matching the given block hash.
+        pub struct GetBlockTransactionCountByHash as "eth_getBlockTransactionCountByHash"
+            (Digest,) => Option<U256>;
+
+        /// Returns the number of transactions in a block matching the given block number.
+        pub struct GetBlockTransactionCountByNumber as "eth_getBlockTransactionCountByNumber"
+            (BlockSpec,) => Option<U256>;
 
         /// Returns code at a given address.
         pub struct GetCode as "eth_getCode"
             (Address, BlockId) => Vec<u8> [serialization::bytes];
+
+        /// Returns a collection of all logs matching the given filter.
+        pub struct GetLogs as "eth_getLogs"
+            (LogFilter,) => Vec<Log>;
+
+        /// Returns information about a transaction by block hash and transaction index position.
+        pub struct GetTransactionByBlockHashAndIndex as "eth_getTransactionByBlockHashAndIndex"
+            (Digest, U256) => Option<SignedTransaction>;
+
+        /// Returns information about a transaction by block number and transaction index position.
+        pub struct GetTransactionByBlockNumberAndIndex as "eth_getTransactionByBlockNumberAndIndex"
+            (BlockSpec, U256) => Option<SignedTransaction>;
+
+        /// Returns information about a transaction requested by transaction hash.
+        pub struct GetTransactionByHash as "eth_getTransactionByHash"
+            (Digest,) => Option<SignedTransaction>;
     }
 }
 

e5a1b546713637292daad71dd05c51c9293589b4
Benjamin Smith <bh2smith@users.noreply.github.com>
2023-11-13 10:29:44 +0100

    Update lib.rs
    Co-authored-by: Nicholas Rodrigues Lordello <n@lordello.net>

--- src/lib.rs
+++ src/lib.rs
@@ -83,7 +83,7 @@ module! {
 
         /// Returns the value from a storage position at a given address.
         pub struct GetStorageAt as "eth_getStorageAt"
-            (Address, U256, Option<BlockSpec>) => [u8; 32] [serialization::bytearray];
+            (Address, U256, Option<BlockId>) => [u8; 32] [serialization::bytearray];
 
         /// Returns information about a transaction by block hash and transaction index position.
         pub struct GetTransactionByBlockHashAndIndex as "eth_getTransactionByBlockHashAndIndex"
